964655dccb4dc858cff75e8500cb9bff6ae38276
Rotem Meiri <rotemx@users.noreply.github.com>
2022-05-14 15:07:31 +0300

    express phase1 - hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-const express = require('express');
+const
+	express = require('express'),
+	app     = express()
+	port = 3000;
+
+
+//happens every time a user connects to get "/"
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+// happens once on each load
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
