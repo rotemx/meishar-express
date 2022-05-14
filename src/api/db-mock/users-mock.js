8ef30cb452afe0b14655ed71fce43cef8790dee6
Rotem Meiri <rotemx@users.noreply.github.com>
2022-05-14 16:02:55 +0300

    express phase3 - authentication

---
+++ src/api/db-mock/users-mock.js
@@ -0,0 +1,9 @@
+ const USERS = [{
+	username:'rotem',
+	password: '12345'
+}]
+
+ 
+ module.exports = {
+	USERS
+ }
--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,24 @@ const
 	corsOptions = {
 		origin: 'http://localhost:4000',
 	}
+const {USERS}   = require("./db-mock/users-mock");
 
 app.use(cors(corsOptions))
+
+
+app.use('/hello', (req, res, next) => {
+	console.log('req.query');
+	console.log(req.query);
+	const {user, password} = req.query;
+	
+	if (USERS.find(item => item.username === user && item.password === password)){
+		next();
+	}
+	else {
+		res.sendStatus(401)
+	}
+	
+})
 
 //happens every time a user connects to get "/"
 app.get('/hello', (req, res) => {
